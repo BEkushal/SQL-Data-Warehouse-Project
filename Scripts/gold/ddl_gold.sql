452c69b8e2d6ddbd866bcb5c370b2da783fd4abc
BEkushal <85442622+BEkushal@users.noreply.github.com>
2025-07-15 23:25:37 +0530

    Update ddl_gold.sql

--- Scripts/gold/ddl_gold.sql
+++ Scripts/gold/ddl_gold.sql
@@ -25,18 +25,18 @@ GO
 
 CREATE VIEW gold.dim_customers AS
 SELECT 
-	    ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key, -- -------->>>>>> Surrogate key
-    	ci.cst_id AS customer_id,
-    	ci.cst_key AS customer_number,
-    	ci.cst_firstname AS first_name,
-    	ci.cst_lastname  AS last_name,
-    	cl.CNTRY AS country,
-    	ci.cst_marital_status AS marital_status,
-	    CASE WHEN ci.cst_gndr <> 'unknown' THEN ci.cst_gndr -- ---------->>>>>> CRM is primary source of truth for gender
-		     ELSE COALESCE(cb.GEN,'unknown') -- --------------------------->>>>>> ERP is secondary source 
-	    END AS gender,
-	    cb.BDATE AS birth_date,
-	    ci.cst_create_date AS create_date
+      ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key, -- -------->>>>>> Surrogate key
+      ci.cst_id AS customer_id,
+      ci.cst_key AS customer_number,
+      ci.cst_firstname AS first_name,
+      ci.cst_lastname  AS last_name,
+      cl.CNTRY AS country,
+      ci.cst_marital_status AS marital_status,
+      CASE WHEN ci.cst_gndr <> 'unknown' THEN ci.cst_gndr -- ---------->>>>>> CRM is primary source of truth for gender
+	   ELSE COALESCE(cb.GEN,'unknown') -- --------------------------->>>>>> ERP is secondary source 
+      END AS gender,
+      cb.BDATE AS birth_date,
+      ci.cst_create_date AS create_date
 FROM silver.crm_cust_info AS ci
 LEFT JOIN silver.erp_cust_az12 AS cb
       ON ci.cst_key = cb.CID
@@ -54,7 +54,7 @@ GO
 CREATE VIEW gold.dim_products AS
 SELECT
       ROW_NUMBER() OVER(ORDER BY pc.prd_start_dt,pc.prd_key) AS product_key, 
-	    pc.prd_id AS product_id, 
+      pc.prd_id AS product_id, 
       pc.prd_key AS product_number,
       pc.prd_nm AS product_name,
       pc.cat_id AS category_id,
