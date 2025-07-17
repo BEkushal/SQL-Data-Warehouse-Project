ed9cc781c53fcf6e3a9c149842ab633f88034f69
BEkushal <85442622+BEkushal@users.noreply.github.com>
2025-07-17 15:28:11 +0530

    Update and rename 07_products_report to 07_products_report.sql

--- Scripts/ADA/07_products_report.sql
+++ Scripts/ADA/07_products_report.sql
@@ -34,9 +34,8 @@ WITH base_query AS (
 1) Base Query: Retrieves core columns from fact_sales and dim_products
 ---------------------------------------------------------------------------*/
     SELECT
-	    f.order_number,
+	f.order_number,
         f.order_date,
-		f.customer_key,
         f.sales_amount,
         f.quantity,
         p.product_key,
@@ -63,10 +62,10 @@ SELECT
     DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan,
     MAX(order_date) AS last_sale_date,
     COUNT(DISTINCT order_number) AS total_orders,
-	COUNT(DISTINCT customer_key) AS total_customers,
+    COUNT(DISTINCT customer_key) AS total_customers,
     SUM(sales_amount) AS total_sales,
     SUM(quantity) AS total_quantity,
-	ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),1) AS avg_selling_price
+    ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),1) AS avg_selling_price
 FROM base_query
 
 GROUP BY
@@ -89,9 +88,9 @@ SELECT
 	last_sale_date,
 	DATEDIFF(MONTH, last_sale_date, GETDATE()) AS recency_in_months,
 	CASE
-		WHEN total_sales > 50000 THEN 'High-Performer'
-		WHEN total_sales >= 10000 THEN 'Mid-Range'
-		ELSE 'Low-Performer'
+	    WHEN total_sales > 50000 THEN 'High-Performer'
+	    WHEN total_sales >= 10000 THEN 'Mid-Range'
+	    ELSE 'Low-Performer'
 	END AS product_segment,
 	lifespan,
 	total_orders,
@@ -101,14 +100,14 @@ SELECT
 	avg_selling_price,
 	-- Average Order Revenue (AOR)
 	CASE 
-		WHEN total_orders = 0 THEN 0
-		ELSE total_sales / total_orders
+	    WHEN total_orders = 0 THEN 0
+	    ELSE total_sales / total_orders
 	END AS avg_order_revenue,
 
 	-- Average Monthly Revenue
 	CASE
-		WHEN lifespan = 0 THEN total_sales
-		ELSE total_sales / lifespan
+	    WHEN lifespan = 0 THEN total_sales
+	    ELSE total_sales / lifespan
 	END AS avg_monthly_revenue
 
 FROM product_aggregations 
