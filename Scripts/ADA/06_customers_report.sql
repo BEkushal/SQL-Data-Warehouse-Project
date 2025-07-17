6ead1c592985a317fc280015dbd94bc6c400f2df
BEkushal <85442622+BEkushal@users.noreply.github.com>
2025-07-17 15:07:44 +0530

    Update 06_customers_report.sql

--- Scripts/ADA/06_customers_report.sql
+++ Scripts/ADA/06_customers_report.sql
@@ -24,38 +24,37 @@ Highlights:
 -- =============================================================================
 -- Create Report: gold.report_customers
 -- =============================================================================
+
 IF OBJECT_ID('gold.report_customers', 'V') IS NOT NULL
     DROP VIEW gold.report_customers;
 GO
 
 CREATE VIEW gold.report_customers AS
 
-WITH base_query AS(
+WITH base AS(
 /*---------------------------------------------------------------------------
 1) Base Query: Retrieves core columns from tables
 ---------------------------------------------------------------------------*/
 SELECT
-f.order_number,
-f.product_key,
-f.order_date,
-f.sales_amount,
-f.quantity,
-c.customer_key,
-c.customer_number,
-CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
-DATEDIFF(year, c.birthdate, GETDATE()) age
-FROM gold.fact_sales f
-LEFT JOIN gold.dim_customers c
-ON c.customer_key = f.customer_key
-WHERE order_date IS NOT NULL)
+	s.order_number,
+	s.product_key,
+	s.order_date,
+	s.sales_amount,
+	s.quantity,
+	c.customer_key,
+	CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+	DATEDIFF(YEAR, c.birth_date, GETDATE()) AS age
+FROM gold.fact_sales AS s
+LEFT JOIN gold.dim_customers AS c
+ON c.customer_key = s.customer_key
+WHERE s.order_date IS NOT NULL),
 
-, customer_aggregation AS (
+customer_aggregations AS(
 /*---------------------------------------------------------------------------
 2) Customer Aggregations: Summarizes key metrics at the customer level
 ---------------------------------------------------------------------------*/
 SELECT 
 	customer_key,
-	customer_number,
 	customer_name,
 	age,
 	COUNT(DISTINCT order_number) AS total_orders,
@@ -63,44 +62,42 @@ SELECT
 	SUM(quantity) AS total_quantity,
 	COUNT(DISTINCT product_key) AS total_products,
 	MAX(order_date) AS last_order_date,
-	DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan
-FROM base_query
+	DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan
+FROM base
 GROUP BY 
 	customer_key,
-	customer_number,
 	customer_name,
 	age
 )
 SELECT
-customer_key,
-customer_number,
-customer_name,
-age,
-CASE 
-	 WHEN age < 20 THEN 'Under 20'
-	 WHEN age between 20 and 29 THEN '20-29'
-	 WHEN age between 30 and 39 THEN '30-39'
-	 WHEN age between 40 and 49 THEN '40-49'
-	 ELSE '50 and above'
-END AS age_group,
-CASE 
-    WHEN lifespan >= 12 AND total_sales > 5000 THEN 'VIP'
-    WHEN lifespan >= 12 AND total_sales <= 5000 THEN 'Regular'
-    ELSE 'New'
-END AS customer_segment,
-last_order_date,
-DATEDIFF(month, last_order_date, GETDATE()) AS recency,
-total_orders,
-total_sales,
-total_quantity,
-total_products
-lifespan,
--- Compuate average order value (AVO)
-CASE WHEN total_sales = 0 THEN 0
-	 ELSE total_sales / total_orders
-END AS avg_order_value,
--- Compuate average monthly spend
-CASE WHEN lifespan = 0 THEN total_sales
-     ELSE total_sales / lifespan
-END AS avg_monthly_spend
-FROM customer_aggregation
+	customer_key,
+	customer_name,
+	age,
+	CASE 
+		 WHEN age < 20 THEN 'Under 20'
+		 WHEN age between 20 and 29 THEN '20-29'
+		 WHEN age between 30 and 39 THEN '30-39'
+		 WHEN age between 40 and 49 THEN '40-49'
+		 ELSE '50 and above'
+	END AS age_group,
+	CASE 
+		WHEN lifespan >= 12 AND total_sales > 5000 THEN 'VIP'
+		WHEN lifespan >= 12 AND total_sales <= 5000 THEN 'Regular'
+		ELSE 'New'
+	END AS customer_segment,
+	total_orders,
+	total_sales,
+	total_quantity,
+	total_products,
+	lifespan,
+	last_order_date,
+	DATEDIFF(month, last_order_date, GETDATE()) AS recency,
+	-- Compuate average order value 
+	CASE WHEN total_sales = 0 THEN 0
+		 ELSE total_sales / total_orders
+	END AS avg_order_value,
+	-- Compuate average monthly spend
+	CASE WHEN lifespan = 0 THEN total_sales
+		 ELSE total_sales / lifespan
+	END AS avg_monthly_spend
+FROM customer_aggregations
